8dc3b358d26ada39538ac0e16c29f590cb84e7bb
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-26 15:24:04 +0900

    create : todo 초기세팅

---
+++ README.md
@@ -0,0 +1,1 @@
+# node.js To do app
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
