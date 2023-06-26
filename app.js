3e4be1eed28b7ab037b4990463fa10628526265e
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-26 17:03:09 +0900

    update : 기본 형식 추가

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
+
+const router = express.Router();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
