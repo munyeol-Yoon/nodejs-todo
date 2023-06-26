ab98e594ee4f28ccdae0a42a2b4b4513d527698c
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-26 19:55:12 +0900

    create : todo 모델 구현

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  value: String, // 할일이 어떤 것인지 확인
+  doneAt: Date, // 할일이 언제 완료 되었는지
+  order: Number, // 몇 번째 할일인지
+});
+
+TodoSchema.virtual("todoId").get(() => this._id.toHexString());
+
+TodoSchema.set("toJSON", {
+  virtuals: true,
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
